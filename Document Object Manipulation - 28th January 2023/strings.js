abaabf42d0f3da6a8e1b343b0e0bfbae9d12d333
Aquib Ajani <aquib@tabdeel.in>
2023-01-29 00:17:00 +0530

    Add files via upload

---
+++ Document Object Manipulation - 28th January 2023/strings.js
@@ -0,0 +1,13 @@
+let username = '   John Doe  ';
+
+let modifiedName = username.trim();
+
+console.log(username.length);
+console.log(modifiedName.length);
+
+console.log(modifiedName.slice(0, 4));
+
+console.log(modifiedName.replace("John", "Jane"));
+
+console.log(modifiedName.toUpperCase());
+console.log(modifiedName.toLowerCase());
