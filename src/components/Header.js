e19ac9716c7f9e9b6d20f09aba3e98fb1479e23f
Aquib Ajani <aquib@tabdeel.in>
2023-03-11 23:15:56 +0530

    Add files via upload

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import "./Header.css";
+
+function Header() {
+  return (
+    <header>
+      <h1>Best blog posts ever!</h1>
+      <button>Read Now</button>
+    </header>
+  );
+}
+
+export default Header;
